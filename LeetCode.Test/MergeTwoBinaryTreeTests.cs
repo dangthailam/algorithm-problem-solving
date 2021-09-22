fe7ad9a06786965f19e09e8c294f1f5d357b3655
DANG Thai lam <dangt@ardian.com>
2021-09-22 17:53:56 +0200

    Merge Two Binary Tree

---
+++ LeetCode.Test/MergeTwoBinaryTreeTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class MergeTwoBinaryTreeTests
+    {
+        [TestMethod()]
+        public void SolveTest()
+        {
+            var root1 = new TreeNode(1, new TreeNode(3, new TreeNode(5)), new TreeNode(2));
+            var root2 = new TreeNode(2, new TreeNode(1, null, new TreeNode(4)), new TreeNode(3, null, new TreeNode(7)));
+            var result = new MergeTwoBinaryTree().Solve(root1, root2);
+        }
+    }
+}
---
+++ LeetCode/MergeTwoBinaryTree.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class MergeTwoBinaryTree
+    {
+        public TreeNode Solve(TreeNode root1, TreeNode root2)
+        {
+            var queue1 = new Queue<TreeNode>();
+            var queue2 = new Queue<TreeNode>();
+
+            queue1.Enqueue(root1);
+            queue2.Enqueue(root2);
+
+            while(queue1.Count > 0 && queue2.Count > 0)
+            {
+                var node1 = queue1.Dequeue();
+                var node2 = queue2.Dequeue();
+
+                node1.val += node2.val;
+
+                if(node1.left != null && node2.left == null)
+                {
+                    node2.left = new TreeNode();
+                }
+                if(node1.left == null && node2.left != null)
+                {
+                    node1.left = new TreeNode();
+                }
+
+                if (node1.right != null && node2.right == null)
+                {
+                    node2.right = new TreeNode();
+                }
+                if (node1.right == null && node2.right != null)
+                {
+                    node1.right = new TreeNode();
+                }
+                if (node1.left != null)
+                    queue1.Enqueue(node1.left);
+                if (node1.right != null)
+                    queue1.Enqueue(node1.right);
+                if (node2.left != null)
+                    queue2.Enqueue(node2.left);
+                if (node2.right != null)
+                    queue2.Enqueue(node2.right);
+            }
+
+            return root1;
+        }
+    }
+}
