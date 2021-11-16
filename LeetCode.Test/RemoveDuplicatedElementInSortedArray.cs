ba9d7d1d69962caf612d60a7a41268a29694bb11
dangthailam <lamtp1989@gmail.com>
2021-11-16 07:02:50 +0100

    Remove duplicated element in sorted array

---
+++ LeetCode.Test/RemoveDuplicatedElementInSortedArray.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    /// <summary>
+    /// Two pointer: 1st pointer which loops the original array, 2nd pointer increment and loops through the target array
+    /// </summary>
+    [TestClass]
+    public class RemoveDuplicatedElementInSortedArray
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int length = nums.Length;
+
+            if (length == 0)
+                return 0;
+
+            int i = 0;
+            int j = 1;
+
+            while (j < length)
+            {
+                if (nums[i] < nums[j])
+                {
+                    i++;
+                    nums[i] = nums[j];
+                }
+
+                j++;
+            }
+            return i;
+
+            // First try
+
+            //int pointer = 999;
+            //for (int i = nums.Length - 1; i >= 0; i--)
+            //{
+            //    if (pointer != nums[i])
+            //    {
+            //        pointer = nums[i];
+            //    }
+            //    else
+            //    {
+            //        for(int j = i; j < nums.Length - 1; j++)
+            //        {
+            //            nums[j] = nums[j + 1];
+            //        }
+            //        nums[nums.Length - 1] = -101;
+            //    }
+            //}
+            //return nums.Count(n => n > -101);
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            int[] nums = new int[] { 1, 1, 2 };
+
+            int[] expectedNums = new int[] { 1, 2 }; // The expected answer with correct length
+
+            int k = RemoveDuplicates(nums); // Calls your implementation
+
+            Assert.AreEqual(k, expectedNums.Length);
+            for (int i = 0; i < k; i++)
+            {
+                Assert.AreEqual(nums[i], expectedNums[i]);
+            }
+        }
+
+
+    }
+}
--- LeetCode.Test/UpdateMatrixTests.cs
+++ LeetCode.Test/UpdateMatrixTests.cs
@@ -9,9 +9,30 @@ namespace LeetCode.Tests
     [TestClass()]
     public class UpdateMatrixTests
     {
+        public class User
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
         [TestMethod()]
         public void SolveTest()
         {
+            var user = new User
+            {
+                Id = 1,
+                Name = "test"
+            };
+
+            var users1 = new List<User>();
+
+            users1.Add(user);
+
+            var users2 = new List<User>();
+
+            users2.Add(user);
+
+            user.Name = "test2";
+
             var mat = new int[3][] {
                 new int[] {0, 0, 1, 1 },
                 new int[] {0, 1, 0, 1 },
